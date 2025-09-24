2decc3897d35b734d740f7d8145b70192c40b233
Nandini-789 <rnandini789366@gmail.com>
2025-09-25 04:52:27 +0530

    dd add emp

--- playwright.config.js
+++ playwright.config.js
@@ -67,11 +67,7 @@ export default defineConfig({
     //   name: 'Microsoft Edge',
     //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
     // },
-    {
-       name: 'Google Chrome',
-      use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-    },
-  ],
+   //
 
   /* Run your local dev server before starting the tests */
   // webServer: {
@@ -79,5 +75,6 @@ export default defineConfig({
   //   url: 'http://localhost:3000',
   //   reuseExistingServer: !process.env.CI,
   // },
+  ]
 });
 
